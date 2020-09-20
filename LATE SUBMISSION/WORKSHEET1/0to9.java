e372545b45bd5fb63e78e2a7aa2d45c7def149c9
Rithiha <69118656+Rithiha@users.noreply.github.com>
2020-09-20 01:40:50 -0700

    Add files via upload

---
+++ LATE SUBMISSION/WORKSHEET1/0to9.java
@@ -0,0 +1,64 @@
+package worksheet.pkg1;
+
+import java.util.Scanner;
+/**
+ *
+ * @author hp.pc
+ */
+public class zeroto9 {
+    public static void main(String args[])
+    {
+    Scanner obj=new Scanner(System.in);
+    System.out.println("Press Key");
+    int a=obj.nextInt();
+    switch(a)
+    {
+    
+        case 0:
+            System.out.println("U pressed : "+a);
+            break;
+            
+        case 1:
+            System.out.println("U pressed : "+a);
+            break;
+            
+        case 2:
+            System.out.println("U pressed : "+a);
+            break;
+            
+        case 3:
+            System.out.println("U pressed : "+a);
+            break;
+            
+        case 4:
+            System.out.println("U pressed : "+a);
+            break;
+            
+        case 5:
+            System.out.println("U pressed : "+a);
+            break;
+            
+        case 6:
+            System.out.println("U pressed : "+a);
+            break;
+            
+        case 7:
+            System.out.println("U pressed : "+a);
+            break;
+            
+        case 8:
+            System.out.println("U pressed : "+a);
+            break;
+            
+        case 9:
+            System.out.println("U pressed : "+a);
+            break;
+         
+        default:
+            System.out.println("U pressed a invalid key ");
+           
+    }
+    
+    }
+    
+}
